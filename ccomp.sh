7dba5ca0fc1e04afebad356877080818e1d9b523
Gustavo Bender <gustavo.benderr@gmail.com>
2023-06-20 13:58:21 -0300

    Shell script for creation of a environment for competitive program in C++

---
+++ ccomp.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+touch ./build.sh
+touch ./main.cpp
+chmod +x build.sh
+#git init
+
+echo "#!/bin/bash" >> ./build.sh
+echo "" >> ./build.sh
+echo "clang++ main.cpp -o main -Wall -O3" >> ./build.sh
+echo "./main" >> ./build.sh
+
+echo "#include <bits/stdc++.h>" >> main.cpp
+echo "" >> main.cpp
+echo "using namespace std;" >> main.cpp
+echo "" >> main.cpp
+echo "int main(int argc, char** argv)" >> main.cpp
+echo "{" >> main.cpp
+echo "   cout << \"Hello World!\" << endl;" >> main.cpp
+echo "" >> main.cpp
+echo "   return 0;" >> main.cpp
+echo "}" >> main.cpp
+
+
