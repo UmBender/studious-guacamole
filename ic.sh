af8ad854105ba9653b5d48a2600bf47d8c24675a
Gustavo Bender <gustavo.benderr@gmail.com>
2023-08-23 22:59:28 -0300

    Creation of a script for studing my cientific iniciation

---
+++ ic.sh
@@ -0,0 +1,3 @@
+
+exec firefox ~/Documentos/minha-IC/IC-DinamicosDiscretos/refs/* https://www.overleaf.com "https://colab.research.google.com" "https://mail.google.com/mail/u/0/#inbox" "https://web.whatsapp.com/" 
+exec obsidian
