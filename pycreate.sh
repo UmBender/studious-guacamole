5be4497386b36752d0393e07140677f9b6f59b63
Gustavo Bender <gustavo.benderr@gmail.com>
2024-01-08 23:46:21 -0300

    Removing nix shell from the script

--- pycreate.sh
+++ pycreate.sh
@@ -1,21 +1,4 @@
-touch build.sh main.py shell.nix
-
-echo "# shell.nix" >> ./shell.nix
-echo "{ pkgs ? import <nixpkgs> {} }:" >> ./shell.nix
-echo "" >> ./shell.nix
-echo "let" >> ./shell.nix
-echo "  python = pkgs.python3;" >> ./shell.nix
-echo "" >> ./shell.nix
-echo "in" >> ./shell.nix
-echo "pkgs.mkShell {" >> ./shell.nix
-echo "  buildInputs = with pkgs; [">> ./shell.nix
-echo "   python" >> ./shell.nix
-echo "    python3Packages.selenium" >> ./shell.nix
-echo "    python3Packages.numpy" >> ./shell.nix
-echo "    python3Packages.matplotlib" >> ./shell.nix
-echo "  ];" >> ./shell.nix
-echo " }" >> ./shell.nix
-
+touch build.sh main.py 
 
 echo "def main():" >> main.py
 echo "    print(\"Hello World\")" >> main.py
@@ -27,6 +10,6 @@ echo "" >> main.py
 
 
 
-echo "nix-shell --command \"python3 main.py\" " >> build.sh
+echo "python3 main.py" >> build.sh
 
 chmod +x build.sh
