75b9ecf85f7d61ea6c1448865b5618ea86b54c3e
Gustavo Bender <gustavo.benderr@gmail.com>
2024-01-20 18:14:30 -0300

    Remaking refs to open only one book

--- refs.sh
+++ refs.sh
@@ -1,9 +1,4 @@
-#cat lang_list | fzf
 var=$(cat /home/bender/scripts/lang_list | fzf)
-valor=$(ls -1 "/home/bender/newlang/$var/refs")
-for i in $valor
-do
-	firefox "/home/bender/newlang/$var/refs/$i"
-done
-
+book=$(ls -1 "/home/bender/newlang/$var/refs" | fzf)
+firefox "/home/bender/newlang/$var/refs/$book"
 
