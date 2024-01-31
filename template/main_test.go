80307347be0ba4420b2fa7138d7784955fc669db
Gustavo Bender <gustavo.benderr@gmail.com>
2024-01-31 16:34:18 -0300

    Template of test file for a go project

---
+++ template/main_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_One(t *testing.T) {
+	result :=
+	expectedResult :=
+	if !reflect.DeepEqual(result, expectedResult) {
+		t.Errorf("Your evaluation was incorrect, got: %v, want: %v", result, expectedResult)
+	}
+}
+
+func Test_Two(t *testing.T) {
+	result :=
+	expectedResult :=
+	if !reflect.DeepEqual(result, expectedResult) {
+		t.Errorf("Your evaluation was incorrect, got: %v, want: %v", result, expectedResult)
+	}
+}
+
+func Test_Three(t *testing.T) {
+	result :=
+	expectedResult :=
+	if !reflect.DeepEqual(result, expectedResult) {
+		t.Errorf("Your evaluation was incorrect, got: %v, want: %v", result, expectedResult)
+	}
+}
+
+func Test_Four(t *testing.T) {
+	result :=
+	expectedResult :=
+	if !reflect.DeepEqual(result, expectedResult) {
+		t.Errorf("Your evaluation was incorrect, got: %v, want: %v", result, expectedResult)
+	}
+}
