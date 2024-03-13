3568a7719aad1513aedd3bb3babd4ec39d717699
Gustavo Bender <gustavo.benderr@gmail.com>
2024-03-13 11:26:37 -0300

    feat: add make file for multiple compiling options

--- ccreate.sh
+++ ccreate.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-touch ./build.sh ./main.c README.md .gitignore
+touch ./build.sh ./main.c README.md .gitignore Makefile
 chmod +x build.sh
 git init
 
 #echo "#!/bin/bash" >> ./build.sh
-echo "gcc main.c -o main -lm -lraylib -lGL -lpthread -ldl -lrt -lX11 -Wall -g -Og -Werror #-save-temps" >> ./build.sh
-echo "./main" >> ./build.sh
+echo "make dev" >> ./build.sh
+echo "./main-dev" >> ./build.sh
 
 DATA=$(cat ~/scripts/template/ccreate.c)
 echo "$DATA" >> main.c
 
+DATA=$(cat ~/scripts/template/Makefilec)
+echo "$DATA" >> Makefile
+
 echo "/main" >> .gitignore
+echo "/*.o" >> .gitignore
