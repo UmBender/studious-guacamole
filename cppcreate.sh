f90686ec42eaaefc37c08fd9ea6ce75af5492bb8
Gustavo Bender <gustavo.benderr@gmail.com>
2024-03-13 11:27:08 -0300

    feat: add makefile for multiple compiling options

--- cppcreate.sh
+++ cppcreate.sh
@@ -1,15 +1,18 @@
-touch build.sh main.cpp .gitignore README.md
-echo "g++ main.cpp -o main -lm -Wall -g -Og -Werror" >> ./build.sh
-echo "./main" >> ./build.sh
+touch build.sh main.cpp .gitignore README.md Makefile
+echo "make dev" >> ./build.sh
+echo "./main-dev" >> ./build.sh
 
 DATA=$(cat ~/scripts/template/cppcreate.cpp)
 echo "$DATA" >> main.cpp
 
+DATA=$(cat ~/scripts/template/Makefilecpp)
+echo "$DATA" >> Makefile
 
 chmod +x build.sh
 git init
 echo "/.ccls-cache" >> .gitignore
 echo "/main" >> .gitignore
+echo "/*.o" >> .gitignore
 
 
 
