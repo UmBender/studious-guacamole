f2289c983fc67dafde3a1484609e6d11a4ef7039
Gustavo Bender <gustavo.benderr@gmail.com>
2024-07-20 14:23:20 -0300

    feat: add binary search template for competitive programing template

--- template/ccomp.cpp
+++ template/ccomp.cpp
@@ -6,6 +6,24 @@ using ll = long long;
 #define int ll
 #define endl (char)10
 
+int bs(vector<int> &arr, int target) {
+  int left = 0;
+  int right = arr.size() - 1;
+  while (left <= right) {
+    int middle = left + (right - left) / 2;
+    if (arr[middle] == target) {
+      return middle;
+    }
+
+    if (arr[middle] > target) {
+      right = middle - 1;
+    } else {
+      left = middle + 1;
+    }
+  }
+  return -1;
+}
+
 int32_t main() {
   ios_base::sync_with_stdio(false);
   cin.tie(0), cout.tie(0);
