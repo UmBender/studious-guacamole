e34a14ec4313d7f51909b00c31a11450f7ac4645
Gustavo Bender <gustavo.benderr@gmail.com>
2025-01-28 23:58:47 -0300

    add: makefn to use overloaded functions on templates

--- template/ccomp.cpp
+++ template/ccomp.cpp
@@ -6,9 +6,10 @@ using namespace std;
 #define endl '\n'
 
 #define MAX_INT ((int64_t)1E18 + 1000)
+#define makefn(fn) [](auto... args) { return fn(args...); }
 
 void solution() {
-
+  
 }
 
 int32_t main() {
