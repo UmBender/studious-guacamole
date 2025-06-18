9e22700b120fe17298381ea8381f2acd36be4b06
Gustavo Bender <gustavo.benderr@gmail.com>
2025-06-18 18:38:35 +0200

    feat: add bench mark utilities and also debug control flow change: some debug flags, adding santizers and removing Werror and pedantic

--- template/build_ccomp.sh
+++ template/build_ccomp.sh
@@ -1,13 +1,33 @@
 question="a"
 combine=false
+#debug="debug"
 
 if [ $combine = true ]; then
 	question_combined=""$question"-combined"
 	python3 expander.py ""$question".cpp"
 	mv combined.cpp ""$question_combined".cpp"
-	make $question_combined
-	./"$question_combined" < input.txt
+	make $debug $question_combined
+
+	echo " " > stats
+	if [ $debug ]; then
+		/usr/bin/time -o stats -v ./"$question_combined-debug" < input.txt  > output.txt
+	else
+		/usr/bin/time -o stats -v ./"$question_combined" < input.txt  > output.txt
+	fi
+	cat output.txt
+	rm output.txt
 else
-	make $question
-	./"$question" < input.txt
+	make $debug $question
+
+	echo " " > stats
+	
+	if [ $debug ]; then
+		/usr/bin/time -o stats -v ./"$question-debug" <input.txt > output.txt
+
+	else
+		/usr/bin/time -o stats -v ./"$question" <input.txt > output.txt
+	fi
+
+	cat output.txt
+	rm output.txt
 fi
